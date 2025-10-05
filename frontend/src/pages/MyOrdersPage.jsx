56d6c9af40afa6357b497133604c534b7cba9a54
Yashsam63 <154651884+Yashsam63@users.noreply.github.com>
2025-10-05 23:41:19 +0530

    Update MyOrdersPage.jsx

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -30,7 +30,7 @@ const MyOrdersPage = () => {
             <h2 className='text-xl sm:text-2xl font-bold mb-6'>
                 My Orders
             </h2>
-            <div className="relative shadow-md sm:rounded-lg overflow-hidden">
+            <div className="relative shadow-md sm:rounded-lg overflow-x-auto">
                 <table className="min-w-full text-left text-gray-500">
                     <thead className="bg-gray-100 text-sm uppercase text-gray-700">
                         <tr>
