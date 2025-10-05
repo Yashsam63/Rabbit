ee8dfdfd9dc125eb202c98c7f0c4b9aa7db3f6c2
Yashsam63 <154651884+Yashsam63@users.noreply.github.com>
2025-10-05 20:54:24 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Slices/authSlice";
 import productReducer from "./Slices/ProductSlice";
-import cartReducer from "./Slices/CartSlice";
+import cartReducer from "./Slices/cartSlice";
 import checkoutReducer from "./Slices/checkoutSlice";
 import orderReducer from "./Slices/orderSlice";
 import adminReducer from "./Slices/adminSlice";
